a12d06d5daaac75edd96eb2f133098be9569d9e4
vladimir <indigosunset00@gmail.com>
2022-09-06 03:40:16 +0600

    ended

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
  new Swiper('.ideaSlider', {
-
+     autoplay: {
+         delay: 3000,
+         disableOnInteraction: false,
+     },
     slidesPerView: 1,
      navigation: {
          nextEl: ".swiper-button-next",
